02b6063ff4a77574db71d11b0cc471071dc61167
bicho cutela <haydendanex@gmail.com>
2026-08-08 00:35:44 -0300

    build: configure Firebase using Google Services plugin
    Simplify Firebase initialization by removing manual configuration logic in favor of the Google Services Gradle plugin. Additionally, update the image URL helper to support more Google Drive link formats.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
   alias(libs.plugins.google.devtools.ksp)
   alias(libs.plugins.roborazzi)
   alias(libs.plugins.secrets)
+  alias(libs.plugins.google.services)
   
 }
 android {
--- app/src/main/java/com/example/util/ImageUrlHelper.kt
+++ app/src/main/java/com/example/util/ImageUrlHelper.kt
@@ -5,17 +5,31 @@ object ImageUrlHelper {
         var cleanUrl = url.trim()
         val fileRegex = Regex("""drive\.google\.com/file/d/([^/]+)/?""")
         val openRegex = Regex("""drive\.google\.com/open\?id=([^&]+)""")
+        val ucRegex = Regex("""drive\.google\.com/uc\?.*id=([^&]+)""")
+        val driveLinkRegex = Regex("""drive\.google\.com/thumbnail\?id=([^&]+)""")
         
         val fileMatch = fileRegex.find(cleanUrl)
         if (fileMatch != null) {
             val id = fileMatch.groupValues[1]
-            return "https://drive.google.com/uc?id=$id"
+            return "https://lh3.googleusercontent.com/d/$id"
         }
         
         val openMatch = openRegex.find(cleanUrl)
         if (openMatch != null) {
             val id = openMatch.groupValues[1]
-            return "https://drive.google.com/uc?id=$id"
+            return "https://lh3.googleusercontent.com/d/$id"
+        }
+
+        val ucMatch = ucRegex.find(cleanUrl)
+        if (ucMatch != null) {
+            val id = ucMatch.groupValues[1]
+            return "https://lh3.googleusercontent.com/d/$id"
+        }
+        
+        val driveLinkMatch = driveLinkRegex.find(cleanUrl)
+        if (driveLinkMatch != null) {
+            val id = driveLinkMatch.groupValues[1]
+            return "https://lh3.googleusercontent.com/d/$id"
         }
         
         return cleanUrl
